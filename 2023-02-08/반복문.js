9f38dcf17114f7e6368395882ab97ff66c159ee1
hyunjun9788 <namhyunjun9788@gmail.com>
2023-02-08 16:23:06 +0900

    [UPDATE] 훈훈한 JS: 반복문을 사용한 텍스트 갱신

--- 2023-02-08/반복문.js
+++ 2023-02-08/반복문.js
@@ -1,7 +1,7 @@
 const messageContainer = document.querySelector('#d-day-message');
 const container = document.querySelector('#d-day-container');
 
-//container.style.display = 'none'
+container.style.display = 'none'
 messageContainer.innerHTML ='<h3>D-Day를 입력해 주세요.</h3>'
 const dateFormMaker = function(){
     const inputYear=document.querySelector('#target-year-input').value;
@@ -21,13 +21,16 @@ const counterMaker = function(){
     //만약, remaining이 0이라면, 타이머가 종료되었습니다. 출력
 
     if(remaining<=0){
-
+        container.style.display='none'
         messageContainer.innerHTML = '<h3>타이머가 종료되었습니다.</h3>'
-
+        messageContainer.style.display = 'flex'
+        return
     }else if(isNaN(remaining)){
-
+        container.style.display='none'
         messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다.</h3>'
         //만약 잘못된 날짜가 들어왔다면, 유효한 시간대가 아닙니다. 출력
+        messageContainer.style.display = 'flex'
+        return;
     }
 
 
@@ -40,24 +43,46 @@ const counterMaker = function(){
     }
 
 
-    const documentObj={
-        days:document.getElementById('days'),
-        hours:document.getElementById('hours'),
-        min:document.getElementById('min'),
-        sec:document.getElementById('sec')
-    }
+
+    const documentArr = ['days', 'hours', 'min', 'sec']
 
     const timeKeys = Object.keys(remainingObj)
-    const docKeys=Object.keys(documentObj)
 
-    for(let i=0; i<timeKeys.length; i=i+1){
-        documentObj[docKeys[i]].textContent = remainingObj[timeKeys[i]]
-    }
+    let i=0;
+    for(let tag of documentArr){
+        document.getElementById(tag).textContent = remainingObj[timeKeys[i]]
+        i++
+    }}
+
+const starter = function (){
+    container.style.display='flex'
+    messageContainer.style.display ='none'
+    counterMaker();
+}
+
+// const documentObj={
+//     days:document.getElementById('days'),
+//     hours:document.getElementById('hours'),
+//     min:document.getElementById('min'),
+//     sec:document.getElementById('sec')
+// }
+//
+// let i =0;
+// for(let key in documentObj){
+//     documentObj[key].textContent= remainingObj[timeKeys[i]]
+//     // i=i+1
+//     i++
+// }
+// ['remainingDate','remainingHours',,,]
+// const docKeys=Object.keys(documentObj)
 
 
-   // documentObj['days'].textContent = remainingObj['remainingDate']
-   //  documentObj['hours'].textContent=remainingObj['remainingHours']
-   //  documentObj['min'].textContent = remainingObj['remainingMin'];
-   //  documentObj['sec'].textContent=remainingObj['remainingSec']
+// for(let i=0; i<timeKeys.length; i=i+1){
+//     documentObj[docKeys[i]].textContent = remainingObj[timeKeys[i]]
+// }
 
-};
+
+// documentObj['days'].textContent = remainingObj['remainingDate']
+//  documentObj['hours'].textContent=remainingObj['remainingHours']
+//  documentObj['min'].textContent = remainingObj['remainingMin'];
+//  documentObj['sec'].textContent=remainingObj['remainingSec']
