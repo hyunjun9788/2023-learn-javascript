a2f3a4b2289e57d415152f80bb349e2d5db2061f
hyunjun9788 <namhyunjun9788@gmail.com>
2023-02-15 17:47:35 +0900

    [ADD] JS: 패스트캠퍼스 배열

---
+++ 2023-02-15/array.js
@@ -0,0 +1,22 @@
+const objects=[
+    {name:'멍멍이'},
+    {name: '야옹이'}
+]
+
+console.log(objects)
+console.log(objects[0])
+console.log(objects[1])
+
+const objects=[
+    {name:'멍멍이'},
+    {name: '야옹이'}
+]
+
+objects.push({
+    name: '멍뭉이'
+})
+console.log(objects.length)
+
+const array = [1,true, {a:1}, [1,2,3,4]]
+
+console.log(array[0])
